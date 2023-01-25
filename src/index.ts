b7b994c89380730ad51b709b1708a61ef0d87fec
Mjfreedom <mbuzina@gmail.com>
2023-01-24 23:28:53 -0300

    :sparkles: feat: Endpoint getAllUsers criado

--- src/index.ts
+++ src/index.ts
@@ -28,3 +28,24 @@ app.get("/ping", async (req: Request, res: Response) => {
         }
     }
 })
+
+app.get("/users", async (req: Request, res: Response) => {
+    try {
+
+        const result = await db("users")
+        res.status(200).send(result)
+        
+    } catch (error) {
+        console.log(error)
+
+        if (req.statusCode === 200) {
+            res.status(500)
+        }
+
+        if (error instanceof Error) {
+            res.send(error.message)
+        } else {
+            res.send("Erro inesperado")
+        }
+    }
+})
