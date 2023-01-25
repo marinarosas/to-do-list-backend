fc4b9938ded406e42869e07cc5621c641c1b2710
Mjfreedom <mbuzina@gmail.com>
2023-01-25 11:48:03 -0300

    :sparkles: feat: Endpoint createUser criado

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 import {db} from './database/knex'
+import { idText } from 'typescript'
+import { TUserDB } from './type'
 
 const app = express()
 
@@ -56,3 +58,78 @@ app.get("/users", async (req: Request, res: Response) => {
         }
     }
 })
+
+app.post("/users", async (req: Request, res: Response) => {
+    try {
+        
+        const {id, name, email, password} = req.body
+
+        if(!id || !name || !email || !password){
+            res.status(400)
+            throw new Error("Id, name, email ou password não informado")
+        }
+
+        if(typeof id !== "string" &&
+        typeof name !== "string" &&
+        typeof email !== "string" &&
+        typeof password !== "string"){
+            res.status(400)
+            throw new Error("Id, name, email e password são strings.")
+        }
+
+        if(id.length <= 3){
+            res.status(400)
+            throw new Error("Id tem que ter pelo menos 4 caratcteres.")
+        }
+
+        if (!password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,12}$/g)) {
+			throw new Error("'password' deve possuir entre 8 e 12 caracteres, com letras maiúsculas e minúsculas e no mínimo um número e um caractere especial")
+		}
+
+        if (!email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)) {
+            throw new Error("Parâmetro 'email' inválido")
+        }
+
+        const [userIdAlreadyExist]: TUserDB[] | undefined = await db("users").where({id: id})
+
+        if(userIdAlreadyExist){
+            res.status(400)
+            throw new Error("Id já existe.")
+        }
+
+        const [userEmailAlreadyExist]: TUserDB[] | undefined = await db("users").where({email: email})
+
+        if(userEmailAlreadyExist){
+            res.status(400)
+            throw new Error("Email já existe.")
+        }
+
+        const newUser: TUserDB = {
+            id,
+            name,
+            email,
+            password
+        }
+
+        await db("users").insert(newUser)
+        res.status(201).send({
+            message: "User criado com sucesso",
+            user: newUser
+        })
+
+
+    } catch (error) {
+        console.log(error)
+
+        if (req.statusCode === 200) {
+            res.status(500)
+        }
+
+        if (error instanceof Error) {
+            res.send(error.message)
+        } else {
+            res.send("Erro inesperado")
+        }
+    }
+})
+
